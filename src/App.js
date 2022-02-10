5b98b1871083ab8c1da1e477ed01c6a526fbf8c1
Alex <a.malihin@it-justice.com>
2022-02-10 14:51:30 +0300

    dependeces

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import {Footer} from "./components/Footer/Footer";
 import {Main} from "./components/Main/Main";
 import {TopArticle} from "./components/TopArticle/TopArticle";
 import {Header} from "./components/Header/Header";
-import {articles} from "./mockStore/articles";
+// import {articles} from "./mockStore/articles";
 
 function App() {
   return (
     <div className="App">
+        {/*<Routs>*/}
+        {/*    {articles.map(article => (*/}
+        {/*            <Router element={<ArticlePage article={article}/>} path={`/main-page/${article.key}`} />*/}
+        {/*    ))}*/}
+        {/*</Routs>*/}
         <Header/>
         <TopArticle/>
         <Main/>
