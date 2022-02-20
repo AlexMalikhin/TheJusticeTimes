ce4867d7901fb326c93cf2150e810b802a8ef73d
Alex <Retho@yandex.ru>
2022-02-20 21:30:06 +0300

    added styles for active links

--- src/components/LogInMenu/LogInMenu.js
+++ src/components/LogInMenu/LogInMenu.js
@@ -1,16 +1,21 @@
-import { Link } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import {Button} from '../Button/Button';
 import styles from './LogInMenu.module.css';
 
 
-export const LogInMenu = ({active, login, style}) => {
+export const LogInMenu = ({login, style}) => {
+
+    const active = ({isActive}) =>{
+
+    }
+
     return (
         <nav className={styles.logout}>
             <ul className={styles.logout_list}>
-                <li className={styles.logout_li}><Link to='./AllArticles'>All articles</Link></li>
-                <li className={styles.logout_li}><Link to='./MyArticles'>My articles</Link></li>
-                <li className={styles.logout_li}><Link to='./AddArticle'>Add article</Link></li>
-                <li className={styles.logout_li}><Link to='./Profile'>Profile</Link></li>
+                <li><NavLink className={nav =>nav.isActive? styles.active : styles.logout_li} to='./AllArticles'>All articles</NavLink></li>
+                <li><NavLink className={nav =>nav.isActive? styles.active : styles.logout_li} to='./MyArticles'>My articles</NavLink></li>
+                <li><NavLink className={nav =>nav.isActive? styles.active : styles.logout_li} to='./AddArticle'>Add article</NavLink></li>
+                <li><NavLink className={nav =>nav.isActive? styles.active : styles.logout_li} to='./Profile'>Profile</NavLink></li>
             </ul>
             <Button click={login} style={style} title='LogOut' disabled={false}/>
         </nav>
